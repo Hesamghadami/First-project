fcce02f66bd43b8212d9a73897867fb96cee6feb
Hesam Gh <110620352+Hesamghadami@users.noreply.github.com>
2023-03-05 18:27:35 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home , contact , about_us , hi
+from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
