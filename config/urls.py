588c1eb1a2990454ce1a1fa73fe0d60699faaf07
Hesam Gh <110620352+Hesamghadami@users.noreply.github.com>
2023-03-07 22:09:10 +0330

    creating an application named 'home'

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contact', contact),
-    path('about_us', about_us),
-    path('hi', hi),
 ]
---
+++ home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.template import loader
+
+from django.http import HttpResponse
+
+
+def home(req):
+    template = loader.get_template('home1.html')
+    return HttpResponse(template.render())
+
+def contact(req):
+    return HttpResponse("Contact us")
+
+def about_us(req):
+    return HttpResponse("About us")
+
+
+def hi(req):
+    return HttpResponse("Hi")
