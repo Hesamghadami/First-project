fb54dca894da01a60c5122651a207c762ca51001
Hesam Gh <110620352+Hesamghadami@users.noreply.github.com>
2023-03-14 01:11:51 +0330

    static and media setting

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.joinpath('/static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.joinpath('/media')
+
+
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+    BASE_DIR/'media',
+]
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
