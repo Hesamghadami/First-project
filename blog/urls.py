ce024f94b6d1666ad85b5712eeb0139e419c5a90
Hesam Gh <110620352+Hesamghadami@users.noreply.github.com>
2023-05-25 01:23:21 +0430

    ep12

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import *
+
+
 app_name = 'blog'
+
+
 urlpatterns = [
     path('', blog_home , name= 'blog_home'),
+    path('/tag/<str:tag>', blog_home , name= 'blog_home_with_tag'),
+    path('/author/<str:username>', blog_home , name= 'blog_home_with_username'),
+    path('/category/<str:cat>', blog_home , name= 'blog_home_with_category'),
+    path('/search/', blog_home , name= 'blog_home_with_search'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
 from .models import *
 
-def blog_home(req):
+def blog_home(req, tag = None,  username = None, cat = None):
     posts = Post.objects.filter(status = True)
+    category = Category.objects.all()
+
+    last_four_posts = posts[:4]
+
+    if tag:
+        posts = Post.objects.filter(tag__name = tag)
+
+    if username:
+        posts = Post.objects.filter(author__username = username)
+    if cat:
+        posts = Post.objects.filter(category__name = cat)
+
+
+    if req.GET.get('search'):
+        posts = Post.objects.filter(content__contains = req.GET.get('search'))
+
+
+
+
+
+
     context = {
-        'posts': posts
+        'posts': posts,
+        'category':category,
+        'last_four_posts':last_four_posts,
     }
     return render(req, 'blog/blog-home.html', context = context)
+
 def blog_single(req):
     return render(req, 'blog/blog-single.html')
+
 
 
 
